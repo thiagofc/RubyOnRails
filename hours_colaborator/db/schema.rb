3f10746d02f62f2e4084e4e0e3d9113423f48c9b
infodeise <infodeise@gmail.com>
2014-08-08 03:45:52 -0300

    Adjust permissions

--- hours_colaborator/db/schema.rb
+++ hours_colaborator/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804234948) do
+ActiveRecord::Schema.define(version: 20140808013409) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20140804234948) do
     t.datetime "updated_at"
     t.integer  "colaborator_id"
     t.boolean  "approved",       default: false
+    t.string   "justify"
   end
 
   create_table "user_apps", force: true do |t|
@@ -73,5 +74,24 @@ ActiveRecord::Schema.define(version: 20140804234948) do
 
   add_index "user_apps", ["email"], name: "index_user_apps_on_email", unique: true, using: :btree
   add_index "user_apps", ["reset_password_token"], name: "index_user_apps_on_reset_password_token", unique: true, using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email",                            default: "", null: false
+    t.string   "encrypted_password",   limit: 128, default: "", null: false
+    t.string   "password_salt",                    default: "", null: false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                    default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
